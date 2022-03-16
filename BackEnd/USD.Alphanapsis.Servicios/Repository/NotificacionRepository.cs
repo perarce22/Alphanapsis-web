751308d9bee914d46b1948eadc41413c06f450ce
Pedro Arce Prada <pedro.arce@itsight.pe>
2022-03-16 15:33:49 -0500

    PAP

--- BackEnd/USD.Alphanapsis.Servicios/Repository/NotificacionRepository.cs
+++ BackEnd/USD.Alphanapsis.Servicios/Repository/NotificacionRepository.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using USD.Alphanapsis.Data;
 
 namespace USD.Alphanapsis.Servicios.Repository
 {
     public class NotificacionRepository
     {
+        private readonly ApplicationDbContext _bd;
+        NLog.Logger logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
     }
 }
