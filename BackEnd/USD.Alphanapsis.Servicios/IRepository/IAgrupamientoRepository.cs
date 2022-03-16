689df0a91db20d07e4bab3bacd719bed49f5b356
Pedro Arce Prada <pedro.arce@itsight.pe>
2022-03-16 15:58:23 -0500

    pap

--- BackEnd/USD.Alphanapsis.Servicios/IRepository/IAgrupamientoRepository.cs
+++ BackEnd/USD.Alphanapsis.Servicios/IRepository/IAgrupamientoRepository.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using USD.Alphanapsis.Dto;
+using USD.Alphanapsis.Entidades;
 
 namespace USD.Alphanapsis.Servicios.Repository
 {
     public interface IAgrupamientoRepository
     {
+        void EliminaAgrupamiento(int id, string usuario);
+        ICollection<Agrupamiento> ListarAgrupamiento(string filtro, bool? estado, int? areaId, int page, int rows, out int nroTotalRegistros);
+        AgrupamientoDto ObtenerAgrupamiento(int Id);
+        int RegistrarAgrupamiento(AgrupamientoDto agrupa);
+
     }
 }
