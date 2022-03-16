ac4b44a99d7d081000ef3a006fe311ff9f70646f
Valeria Cuellar Portella <valeria.cuellar@itsight.pe>
2022-03-15 23:06:29 -0500

    vrc

---
+++ FrontEnd/app/pages/_app.js
@@ -0,0 +1,19 @@
+import '../styles/globals.css'
+import '../styles/globals.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Layout from "../components/layout"
+
+function MyApp({ Component, pageProps }) {  
+  const LayoutComponent = Component.Layout || EmptyLayout;
+  return (
+    <>
+      <LayoutComponent>
+        <Component {...pageProps} />
+      </LayoutComponent>
+    </>
+  )
+}
+
+const EmptyLayout = ({children}) => <>{children}</>
+
+export default MyApp
